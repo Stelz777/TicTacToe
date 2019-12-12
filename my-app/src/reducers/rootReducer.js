f4dcccb4da9d2325fbd992db91bc2029ce7b9d82
Stelz777 <profflastelz@yandex.ru>
2019-12-12 13:32:25 +0300

    Replaced Object.assign with Object Spread
    В методе rootReducer

--- my-app/src/reducers/rootReducer.js
+++ my-app/src/reducers/rootReducer.js
@@ -15,11 +15,11 @@ function rootReducer(state = initialState, action)
     switch (action.type)
     {
         case HISTORY_ITEM_CLICKED:
-            return Object.assign({}, state, { highlights: action.highlightsInput, xIsNext: (action.stepInput % 2) === 0, stepNumber: action.stepInput });
+            return ({ ...state, highlights: action.highlightsInput, xIsNext: (action.stepInput % 2) === 0, stepNumber: action.stepInput });
         case HISTORY_BUTTON_SWITCHED:
-            return Object.assign({}, state, { history: state.history.slice().reverse(), reverseIsChecked: !state.reverseIsChecked });
+            return ({ ...state, history: state.history.slice().reverse(), reverseIsChecked: !state.reverseIsChecked });
         case GAME_BOARD_CLICKED:
-            return Object.assign({}, state, { history: action.history.concat([{ squares: action.squares, }]), stepNumber: action.history.length, xIsNext: !state.xIsNext });
+            return ({ ...state, history: action.history.concat([{ squares: action.squares, }]), stepNumber: action.history.length, xIsNext: !state.xIsNext });
         default: 
             return state;
     }
