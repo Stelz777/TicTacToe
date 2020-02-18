fe1c65f6a00a7f197d36af62c2c0eb2b67771213
Stelz777 <profflastelz@yandex.ru>
2020-02-18 16:38:06 +0300

    Actions refactored!
    -Лишние действия
    +Алфавитный порядок

--- ASP.NET CoreTicTacToe/my-app/src/actions/actions.js
+++ ASP.NET CoreTicTacToe/my-app/src/actions/actions.js
@@ -1,133 +1,30 @@
 //Action Type
-
+export const ALL_GAMES_RECEIVED = 'ALL_GAMES_RECEIVED'
+export const BOARD_REQUESTED = 'BOARD_REQUESTED';
+export const BOT_IS_X = 'BOT_IS_X';
+export const BOT_O_BUTTON_SWITCHED = 'BOT_O_BUTTON_SWITCHED';
+export const BOT_SET = 'BOT_SET';
+export const BOT_X_BUTTON_SWITCHED = 'BOT_X_BUTTON_SWITCHED';
+export const GAME_BOARD_CLICKED = 'GAME_BOARD_CLICKED';
+export const GAME_INIT = 'GAME_INIT';
 export const HISTORY_BUTTON_SWITCHED = 'HISTORY_BUTTON_SWITCHED'
 export const HISTORY_ITEM_CLICKED = 'HISTORY_ITEM_CLICKED';
-export const GAME_BOARD_CLICKED = 'GAME_BOARD_CLICKED';
-export const BOT_TURN_REQUESTED = 'BOT_TURN_REQUESTED';
-export const BOT_TURN_RESPONDED = 'BOT_TURN_RESPONDED';
-export const BOARD_REQUESTED = 'BOARD_REQUESTED';
 export const HISTORY_REQUESTED = 'HISTORY_REQUESTED';
-export const NAME_SET = 'NAME_SET';
-export const BOT_SET = 'BOT_SET';
-export const BOT_IS_X = 'BOT_IS_X';
-export const PLAYER_NAMES_RECEIVED = 'PLAYER_NAMES_RECEIVED';
-export const SPECTATOR_RESOLVED = 'SPECTATOR_RESOLVED';
-export const GAME_INIT = 'GAME_INIT';
-export const ALL_GAMES_RECEIVED = 'ALL_GAMES_RECEIVED'
 export const LOBBY_INIT = 'LOBBY_INIT';
-export const SIDE_RECEIVED = 'SIDE_RECEIVED';
-export const TEST = 'TEST';
+export const NAME_SET = 'NAME_SET';
 export const NAME_SET_IN_LOBBY = 'NAME_SET_IN_LOBBY';
-export const BOT_X_BUTTON_SWITCHED = 'BOT_X_BUTTON_SWITCHED';
-export const BOT_O_BUTTON_SWITCHED = 'BOT_O_BUTTON_SWITCHED';
+export const PLAYER_NAMES_RECEIVED = 'PLAYER_NAMES_RECEIVED';
+export const SIDE_RECEIVED = 'SIDE_RECEIVED';
+export const SPECTATOR_RESOLVED = 'SPECTATOR_RESOLVED';
 
-export function botXButtonSwitched()
-{
-    return {
-        type: BOT_X_BUTTON_SWITCHED
-    }
-}
 
-export function botOButtonSwitched()
-{
-    return {
-        type: BOT_O_BUTTON_SWITCHED
-    }
-}
-
-export function nameSetInLobby(playerNameInLobby)
-{
-    console.log("action nameSetInLobby: ", playerNameInLobby);
-    return {
-        type: NAME_SET_IN_LOBBY,
-        playerNameInLobby
-    }
-}
-
-export function nameSet(clientPlayerName)
-{
-    console.log("action nameSet playerName: ", clientPlayerName);
-    return {
-        type: NAME_SET,
-        clientPlayerName
-    }
-}
-
-export function test()
-{
-    return {
-        type: TEST
-    }
-}
-
-export function sideReceived(side)
-{
-    return {
-        type: SIDE_RECEIVED,
-        side
-    }
-}
-
-export function lobbyInit()
-{
-    return {
-        type: LOBBY_INIT
-    }
-}
-
+//Action Creator
 export function allGamesReceived(games)
 {
+    //Action
     return {
         type: ALL_GAMES_RECEIVED,
         games
-    }
-}
-
-export function gameInit()
-{
-    return {
-        type: GAME_INIT
-    }
-}
-
-export function spectatorResolved()
-{
-    return {
-        type: SPECTATOR_RESOLVED
-    }
-}
-
-export function playerNamesReceived(ticName, tacName)
-{
-    console.log("playerNamesReceived ticName: ", ticName);
-    console.log("playerNamesReceived tacName: ", tacName);
-    return {
-        type: PLAYER_NAMES_RECEIVED,
-        ticName,
-        tacName
-    }
-}
-
-export function botIsX()
-{
-    return {
-        type: BOT_IS_X
-    }
-}
-
-export function botSet(bot)
-{
-    return {
-        type: BOT_SET,
-        bot
-    }
-}
-
-export function historyRequested(boards)
-{
-    return {
-        type: HISTORY_REQUESTED,
-        boards
     }
 }
 
@@ -139,10 +36,53 @@ export function boardRequested(board)
     }
 }
 
-//Action Creator
+export function botIsX()
+{
+    return {
+        type: BOT_IS_X
+    }
+}
+
+export function botOButtonSwitched()
+{
+    return {
+        type: BOT_O_BUTTON_SWITCHED
+    }
+}
+
+export function botSet(bot)
+{
+    return {
+        type: BOT_SET,
+        bot
+    }
+}
+
+export function botXButtonSwitched()
+{
+    return {
+        type: BOT_X_BUTTON_SWITCHED
+    }
+}
+
+export function gameBoardClicked(squareIndex, side)
+{
+    return {
+        type: GAME_BOARD_CLICKED,
+        squareIndex,
+        side
+    }
+}
+
+export function gameInit()
+{
+    return {
+        type: GAME_INIT
+    }
+}
+
 export function historyButtonSwitched()
 {
-    //Action
     return {
         type: HISTORY_BUTTON_SWITCHED
     }
@@ -157,15 +97,74 @@ export function historyItemClicked(stepInput, squareIndex)
     }
 }
 
-export function gameBoardClicked(squareIndex, side)
+export function historyRequested(boards)
 {
-    
     return {
-        type: GAME_BOARD_CLICKED,
-        squareIndex,
+        type: HISTORY_REQUESTED,
+        boards
+    }
+}
+
+export function lobbyInit()
+{
+    return {
+        type: LOBBY_INIT
+    }
+}
+
+export function nameSet(clientPlayerName)
+{
+    return {
+        type: NAME_SET,
+        clientPlayerName
+    }
+}
+
+export function nameSetInLobby(playerNameInLobby)
+{
+    return {
+        type: NAME_SET_IN_LOBBY,
+        playerNameInLobby
+    }
+}
+
+export function playerNamesReceived(ticName, tacName)
+{
+    return {
+        type: PLAYER_NAMES_RECEIVED,
+        ticName,
+        tacName
+    }
+}
+
+export function sideReceived(side)
+{
+    return {
+        type: SIDE_RECEIVED,
         side
     }
 }
+
+export function spectatorResolved()
+{
+    return {
+        type: SPECTATOR_RESOLVED
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
