85a2e50b455e78c5db26135705fed2b7e9969968
Stelz777 <profflastelz@yandex.ru>
2020-02-28 14:23:01 +0300

    Added reverse agile mapping for bot
    Пофиксить игру с ботом после перезахода в игру!

---
+++ ASP.NET CoreTicTacToe/Infrastructure/MapperProfiles/BotDTOToIBotConverter.cs
@@ -0,0 +1,36 @@
+﻿using ASP.NETCoreTicTacToe.Infrastructure.DTO;
+using ASP.NETCoreTicTacToe.Models.Bots;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ASP.NETCoreTicTacToe.Infrastructure.MapperProfiles
+{
+    public class BotDTOToIBotConverter : IValueConverter<PlayerDataTransferObject, IBot>
+    {
+        public IBot Convert(PlayerDataTransferObject source, ResolutionContext context)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+            var botTypes = (from type in Assembly.GetExecutingAssembly().GetExportedTypes()
+                            where !type.IsInterface && !type.IsAbstract
+                            where typeof(IBot).IsAssignableFrom(type)
+                            select type).ToArray();
+            var instantiatedBots =
+                botTypes.Select(type => (IBot)Activator.CreateInstance(type, source.Side)).ToArray();
+            foreach (var bot in instantiatedBots)
+            {
+                if (bot.Name == source.Difficulty)
+                {
+                    return bot;
+                }
+            }
+            return null;
+        }
+    }
+}
--- ASP.NET CoreTicTacToe/Infrastructure/MapperProfiles/PlayerProfile.cs
+++ ASP.NET CoreTicTacToe/Infrastructure/MapperProfiles/PlayerProfile.cs
@@ -14,11 +14,8 @@ namespace ASP.NETCoreTicTacToe.Infrastructure.MapperProfiles
                 .ForMember(data => data.Difficulty, options => options.MapFrom(
                     map => map.Bot != null ? map.Bot.Name : null))
                 .ReverseMap()
-                .ForPath(data => data.Bot, options => options.MapFrom(
-                    map => map.IsBot ? map.Difficulty == "Simple" ? (IBot) new SimpleBot(map.Side)
-                                                                  : map.Difficulty == "Minimax" ? (IBot) new MinimaxBot(map.Side) 
-                                                                                                : null
-                                     : null));
+                .ForMember(data => data.Bot, options => options.ConvertUsing(
+                    new BotDTOToIBotConverter(), playerDTO => playerDTO));
         }   
     }
 }
