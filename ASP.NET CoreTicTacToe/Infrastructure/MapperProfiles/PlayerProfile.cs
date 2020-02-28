d29bef267a9e8612bcb9d73bdb0354a25b975335
Stelz777 <profflastelz@yandex.ru>
2020-02-28 13:01:06 +0300

    Added direct agile mapping for difficulty

--- ASP.NET CoreTicTacToe/Infrastructure/MapperProfiles/PlayerProfile.cs
+++ ASP.NET CoreTicTacToe/Infrastructure/MapperProfiles/PlayerProfile.cs
@@ -12,7 +12,7 @@ namespace ASP.NETCoreTicTacToe.Infrastructure.MapperProfiles
             CreateMap<Player, PlayerDataTransferObject>()
                 .ForMember(data => data.IsBot, options => options.MapFrom(map => map.Bot != null ? true : false))
                 .ForMember(data => data.Difficulty, options => options.MapFrom(
-                    map => map.Bot is SimpleBot ? "Simple" : map.Bot is MinimaxBot ? "Minimax" : null))
+                    map => map.Bot != null ? map.Bot.Name : null))
                 .ReverseMap()
                 .ForPath(data => data.Bot, options => options.MapFrom(
                     map => map.IsBot ? map.Difficulty == "Simple" ? (IBot) new SimpleBot(map.Side)
